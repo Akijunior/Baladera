3469ad9bcc0bbcb7e94e65e7a2fdb2332a3910d2
Akijunior <suitsu19@gmail.com>
2018-08-08 22:33:26 -0300

    Adição de envio de arquivos via TCP.

---
+++ baladera_app/src/TCPClientTransfer.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.net.*;
+
+public class TCPClientTransfer {
+	public static void main(String[] args) throws Exception {
+		
+		Socket socket = new Socket("localhost", 4500);
+		InputStream inputStream = socket.getInputStream();
+		FileOutputStream outputStream = 
+				new FileOutputStream("C:\\Users\\ljuni\\Downloads\\nova.jpeg");
+		
+		byte[] bytes = new byte[204800];
+		inputStream.read(bytes, 0, bytes.length);
+		outputStream.write(bytes, 0, bytes.length);
+		
+	}
+
+}
